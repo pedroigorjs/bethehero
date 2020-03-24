1218caf527bfcf23527d2407dc267c1e671206bd
Pedro Sampaio <pedroigorjs@gmail.com>
2020-03-24 14:04:11 -0300

    Feature: Setting knexfile.js

---
+++ backend/knexfile.js
@@ -0,0 +1,46 @@
+require('dotenv/config');
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite',
+    },
+    migrations: {
+      directory: './src/database/migrations',
+    },
+    useNullAsDefault: true,
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
