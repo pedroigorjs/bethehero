fc724b8ea6e34c17c2b325abb073ea1b864c895f
Pedro Sampaio <pedroigorjs@gmail.com>
2020-03-24 15:32:06 -0300

    Feature: Create routes with controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,21 @@
 import { Router } from 'express';
 
+import NgoController from './app/controllers/NgoController';
+import IncidentController from './app/controllers/IncidentController';
+import ProfileController from './app/controllers/ProfileController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.send({ ok: true });
-});
+routes.get('/ngos', NgoController.index);
+routes.post('/ngos', NgoController.store);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.destroy);
+
+routes.get('/profiles', ProfileController.index);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
