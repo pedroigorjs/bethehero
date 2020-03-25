8332149edf9467fa7560c4ec1c39ff4ec7f069fe
Pedro Sampaio <pedroigorjs@gmail.com>
2020-03-25 14:12:58 -0300

    Feature: Create Header component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../assets/svg/logo.svg';
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Be The Hero" />
+    </header>
+  );
+}
