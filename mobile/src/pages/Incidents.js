eacf917bd64c197e8d2cb40e8df18038d1867613
Pedro Sampaio <pedroigorjs@gmail.com>
2020-03-26 22:14:42 -0300

    Feature: Create Incident page with styles

--- mobile/src/pages/Incidents.js
+++ mobile/src/pages/Incidents.js
@@ -1,6 +1,56 @@
 import React from 'react'
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+
+import logo from '../assets/logo.png'
+
+import styles from './incidents/styles'
+
 const Incidents = () => {
+  const navigation = useNavigation()
+
+  function navigateToDetails() {
+    navigation.navigate('Details')
+  }
+
   return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logo} />
+        <Text style={styles.headerText}>
+          Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Bem vindo!</Text>
+      <Text style={styles.description}>Escolha um dos casos abaixo
+e salve o dia.</Text>
+
+      <FlatList 
+        data={[1, 2, 3]}
+        style={styles.incidentsList}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incidentItem}>
+            <View style={styles.incidentDetails}>
+              <Text style={styles.incidentProperty}>Caso:</Text>
+              <Text style={styles.incidentValue}>Cadelinha Atropelada 1</Text>
+              <Text style={styles.incidentProperty}>ONG:</Text>
+              <Text style={styles.incidentValue}>APAE Cotia</Text>
+              <Text style={styles.incidentProperty}>Valor:</Text>
+              <Text style={styles.incidentValue}>R$ 120,00</Text>
+            </View>
+
+            <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetails}>
+              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Feather name="arrow-right" size={24} color="#e02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
   )
 }
 
---
+++ mobile/src/pages/incidents/styles.js
@@ -0,0 +1,82 @@
+import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
+    backgroundColor: '#f0f0f5',
+  },
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  headerText: {
+    fontSize: 15,
+    color: '#737380',
+  },
+
+  headerTextBold: {
+    fontWeight: 'bold',
+  },
+
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#13131A',
+    marginTop: 50,
+    marginBottom: 15,
+  },
+
+  description: {
+    fontSize: 20,
+    color: '#737380',
+  },
+
+  incidentsList: {
+    marginTop: 30,
+  },
+
+  incidentItem: {
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+    marginBottom: 15,
+  },
+
+  incidentDetails: {
+    paddingHorizontal: 25,
+    paddingVertical: 25,
+  },
+
+  incidentProperty: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: '#41414d',
+    textTransform: 'uppercase',
+  },
+
+  incidentValue: {
+    color: '#737380',
+    marginBottom: 20,
+  },
+
+  detailsButton: {
+    flexDirection: 'row',
+    paddingHorizontal: 25,
+    paddingVertical: 25,
+    borderTopWidth: 1,
+    borderTopColor: '#f0f0f5',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  detailsButtonText: {
+    color: '#e02041',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+})
