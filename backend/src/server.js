e99805b24e869ea2ec0fe0c662d683482426a393
Pedro Sampaio <pedroigorjs@gmail.com>
2020-03-27 16:39:49 -0300

    Feature: Add errors from celebrate in the project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import 'dotenv/config';
 import app from './app';
+import { errors } from 'celebrate'
 
 const { HOST, PORT } = process.env;
+
+app.use(errors())
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://${HOST}:${PORT}`);
